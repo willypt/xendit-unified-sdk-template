5c10d93862e3ced2cd2c350af1b7208ae9741185
Willy PT <1708055+willypt@users.noreply.github.com>
2023-07-05 01:36:11 +0700

    feat: generate client but not correct and want to change still

---
+++ clients/typescript/api/apis.ts
@@ -0,0 +1,14 @@
+export * from './balancesApi';
+import { BalancesApi } from './balancesApi';
+import * as http from 'http';
+
+export class HttpError extends Error {
+    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
+        super('HTTP request failed');
+        this.name = 'HttpError';
+    }
+}
+
+export { RequestFile } from '../model/models';
+
+export const APIS = [BalancesApi];
